c6bb089d28523ce24ce5493ed62a5ca22e3d88ad
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-03-07 00:49:35 +0800

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,12 +1,14 @@
-
 import { supabase } from "./supabase.js"
 import { getGPS } from "./gps.js"
 import { getLocation } from "./location.js"
+import { saveOffline, getOffline, clearOffline } from "./offline.js"
 
 export async function clock(){
 
 const emp_id = localStorage.getItem("emp_id")
 const device = localStorage.getItem("device_guid")
+
+try{
 
 const gps = await getGPS()
 const address = await getLocation(gps.lat,gps.lng)
@@ -24,9 +26,8 @@ alert("Maximum logs reached today")
 return
 }
 
-await supabase
-.from("attendance_logs")
-.insert({
+const log={
+
 emp_id:emp_id,
 log_time:new Date(),
 latitude:gps.lat,
@@ -35,10 +36,52 @@ accuracy:gps.accuracy,
 address:address,
 device_id:device,
 device_type:"MOBILE_WEB"
-})
+
+}
+
+// if online save immediately
+if(navigator.onLine){
+
+await supabase
+.from("attendance_logs")
+.insert(log)
+
+}else{
+
+// save offline
+saveOffline(log)
+alert("Offline mode: log saved locally")
+
+}
+
+syncOffline()
 
 alert("Attendance recorded")
 
 location.reload()
+
+}catch(e){
+
+alert("GPS Error: "+e)
+
+}
+
+}
+
+async function syncOffline(){
+
+if(!navigator.onLine) return
+
+const logs = getOffline()
+
+for(let log of logs){
+
+await supabase
+.from("attendance_logs")
+.insert(log)
+
+}
+
+clearOffline()
 
 }
