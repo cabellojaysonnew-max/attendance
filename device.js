6ed09bae28e5892bc04c67d7c32e4ff300331ccf
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-03-07 16:55:47 +0800

    Update device.js

--- device.js
+++ device.js
@@ -10,11 +10,14 @@ navigator.platform +
 screen.width + "x" + screen.height +
 Intl.DateTimeFormat().resolvedOptions().timeZone
 
-device = crypto.randomUUID() + "-" + btoa(fingerprint)
+const encoded = btoa(fingerprint)
+
+device = crypto.randomUUID() + "-" + encoded
 
 localStorage.setItem("device_guid",device)
 
 }
 
 return device
+
 }
