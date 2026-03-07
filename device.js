32748e413f8908fd400dc00fdfbc73c63e62cb89
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-03-07 16:51:07 +0800

    Update device.js

--- device.js
+++ device.js
@@ -1,11 +1,19 @@
-
 export function getDevice(){
 
 let device = localStorage.getItem("device_guid")
 
 if(!device){
- device = crypto.randomUUID()
- localStorage.setItem("device_guid",device)
+
+const fingerprint =
+navigator.userAgent +
+navigator.platform +
+screen.width + "x" + screen.height +
+Intl.DateTimeFormat().resolvedOptions().timeZone
+
+device = crypto.randomUUID() + "-" + btoa(fingerprint)
+
+localStorage.setItem("device_guid",device)
+
 }
 
 return device
