c48d87eb4f212d777f5080057b63178e8efc7d0d
cabellojaysonnew-max <cabello.jayson.new@gmail.com>
2026-03-07 16:56:15 +0800

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,31 +1,26 @@
 import { supabase } from "./supabase.js"
 import { clock } from "./clock.js"
 
-const emp_id = localStorage.getItem("emp_id")
-const emp_name = localStorage.getItem("emp_name")
+const emp_id=localStorage.getItem("emp_id")
+const emp_name=localStorage.getItem("emp_name")
 
-document.getElementById("empName").innerText = emp_name
-document.getElementById("clockBtn").onclick = clock
+document.getElementById("empName").innerText=emp_name
+document.getElementById("clockBtn").onclick=clock
 
 loadLogs()
 
 async function loadLogs(){
 
-const start = new Date()
-start.setHours(0,0,0,0)
+const today=new Date().toISOString().split("T")[0]
 
-const end = new Date()
-end.setHours(23,59,59,999)
-
-const { data } = await supabase
+const {data}=await supabase
 .from("attendance_logs")
 .select("*")
 .eq("emp_id",emp_id)
-.gte("log_time", start.toISOString())
-.lte("log_time", end.toISOString())
+.gte("log_time",today)
 .order("log_time",{ascending:true})
 
-render(data || [])
+render(data||[])
 
 }
 
@@ -35,15 +30,30 @@ let html=""
 
 logs.forEach((log,index)=>{
 
-let color = (index % 2 === 0) ? "green" : "red"
+let color=(index%2===0)?"green":"red"
 
-html += `<div class="log ${color}">
+html+=`<div class="log ${color}">
 ${new Date(log.log_time).toLocaleTimeString()}<br>
-${log.place_name || log.address}
+${log.place_name||log.address}
 </div>`
 
 })
 
-document.getElementById("logs").innerHTML = html
+document.getElementById("logs").innerHTML=html
 
 }
+
+/* -------- DEVTOOLS DETECTION -------- */
+
+setInterval(()=>{
+
+const devtoolsOpen =
+window.outerWidth - window.innerWidth > 160 ||
+window.outerHeight - window.innerHeight > 160
+
+if(devtoolsOpen){
+ alert("Developer tools detected")
+ location.href="index.html"
+}
+
+},1000)
